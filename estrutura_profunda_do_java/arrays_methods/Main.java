80bd1ea9b90342a114b192194d1a7f092764b14b
BAIANO6969 <>
2026-01-04 13:21:23 +0000

    Arrays methods

---
+++ estrutura_profunda_do_java/arrays_methods/Main.java
@@ -0,0 +1,17 @@
+package com.example;
+
+// import the Arrays library here
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        char[] unsortedArray = {'y', 'e', 'e', 'p', 'w', 'c', '>', '3'};
+       
+        // sort the array and output it to the console
+        Arrays.sort(unsortedArray);
+        for (char element : unsortedArray) {
+            System.out.print(element + "");
+        }
+    }
+}
